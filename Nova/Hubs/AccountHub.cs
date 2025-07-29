d07526e781af7c5d83addda85e334b1d8ebaff1e
David Tilman <D.Tilman05@gmail.com>
2025-07-29 13:45:10 +0100

    add web client

---
+++ Nova/Hubs/AccountHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+using Nova.Database;
+
+namespace Nova.Hubs;
+internal class AccountHub : Hub
+{
+    public async Task<List<Account>> GetAccountsAsync() => await AccountManager.GetAccountsAsync();
+}
